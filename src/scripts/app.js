3e21491cdae56f0a52f038e29e6f3fe45c23f694
mkazemiraz <kazemiraz72@gmail.com>
2020-08-21 22:17:30 +0430

    sprint 1399.06.01 completed!

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,36 @@
 $(document).ready(function() {
-    var seocnd = 60;
-    setInterval(function() {
-        seocnd--;
-        $('#second').html(seocnd);
-        seocnd = (seocnd == 0) ? 60 : seocnd;
-    }, 1000);
+
+
+    function getTimeRemaining(endtime){
+        var total = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor( (total/1000) % 60 );
+        var minutes = Math.floor( (total/1000/60) % 60 );
+        var hours = Math.floor( (total/(1000*60*60)) % 24 );
+        var days = Math.floor( total/(1000*60*60*24) );
+      
+        return {
+          totle: total,
+          days: days,
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds
+        };
+    }
+    var timeinterval = setInterval(function(){
+        var t = getTimeRemaining("20 Jan 2021 00:00:00 GMT");
+        $('#days').html(t.days);
+        $('#hours').html(t.hours);
+        $('#minutes').html(t.minutes);
+        $('#seconds').html(t.seconds);
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+    },1000);
+
+
+    $('.dropdown-toggle').click(function() {
+        $(this).next('.dropdown-menu').slideToggle(250);
+    });
+    AOS.init();
 });
 
